69221cf3acf3f3da4483b9cb4f87746cb706f8f4
katsuchi23 <reynaldywidjaja3@gmail.com>
2025-11-04 16:37:32 +0800

    add livox  custom msg and pointcloud to laser scan

--- navigation/g1_navigation/launch/g1_navigation.launch.py
+++ navigation/g1_navigation/launch/g1_navigation.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
     # Declare and launch configuration
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
-        default_value=os.path.join(nav2_bringup_dir, 'maps', 'dqn_stage1.yaml'),
+        default_value=os.path.join(nav2_bringup_dir, 'maps', 'map.yaml'),
         description='Full path to map yaml file to load')
     
     declare_autostart_cmd = DeclareLaunchArgument(
